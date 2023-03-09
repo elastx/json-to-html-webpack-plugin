a6d26a5b968a749b28eb4f70a5225afc34c6b487
Mikael Grön <skaramicke@gmail.com>
2023-03-09 11:01:28 +0100

    Add ability to process multiple pages instead of one

--- index.js
+++ index.js
@@ -5,41 +5,90 @@ const ejs = require("ejs");
 class JSONToHTMLWebpackPlugin {
   constructor(options) {
     this.options = options;
+    this.timestamps = new Map();
   }
 
   apply(compiler) {
     const { webpack } = compiler;
-    const { Compilation } = webpack;
     const { RawSource } = webpack.sources;
 
     compiler.hooks.thisCompilation.tap(
       "JSONToHTMLWebpackPlugin",
       (compilation) => {
+        this.options.pages.forEach((page) => {
+          // Add the template file to deps
+          if (!compilation.fileDependencies.has(path.resolve(page.template))) {
+            compilation.fileDependencies.add(path.resolve(page.template));
+          }
+          page.jsonFiles.forEach((jsonFile) => {
+            // Add each json file to deps
+            if (!compilation.fileDependencies.has(path.resolve(jsonFile))) {
+              compilation.fileDependencies.add(path.resolve(jsonFile));
+            }
+          });
+        });
+
         compilation.hooks.processAssets.tap(
           {
             name: "JSONToHTMLWebpackPlugin",
             stage: compilation.PROCESS_ASSETS_STAGE_ADDITIONS,
           },
           () => {
-            const rawData = {};
-            this.options.jsonFiles.forEach((jsonFile) => {
-              const jsonFilePath = path.resolve(jsonFile);
-              const jsonData = JSON.parse(
-                fs.readFileSync(jsonFilePath, "utf8")
+            const dependencies = this.options.pages
+              .reduce(
+                (acc, page) => [...acc, ...page.jsonFiles, page.template],
+                []
+              )
+              .map((file) => path.resolve(file));
+
+            const changedDependencies = dependencies.filter((file) => {
+              return (
+                !this.timestamps.has(file) ||
+                this.timestamps.get(file) < fs.statSync(file).mtimeMs
               );
-              rawData[path.basename(jsonFilePath, ".json")] = jsonData;
             });
 
-            const data = this.options.transformer(rawData);
+            changedDependencies.forEach((file) => {
+              this.timestamps.set(file, fs.statSync(file).mtimeMs);
+            });
 
-            const htmlFilePath = `${this.options.target}index.html`;
+            // Get all pages that depend on changed files
+            const pagesWithChangedDependencies = this.options.pages.filter(
+              (page) => {
+                return page.jsonFiles.some((jsonFile) => {
+                  return changedDependencies.includes(path.resolve(jsonFile));
+                });
+              }
+            );
 
-            ejs.renderFile(this.options.template, data, (err, html) => {
-              if (err) {
-                throw err;
+            // Render each page
+            pagesWithChangedDependencies.forEach((page) => {
+              const { jsonFiles, transformer, template, target } = page;
+
+              const rawData = {};
+              jsonFiles.forEach((jsonFile) => {
+                const jsonFilePath = path.resolve(jsonFile);
+                const jsonData = JSON.parse(
+                  fs.readFileSync(jsonFilePath, "utf8")
+                );
+                rawData[path.basename(jsonFilePath, ".json")] = jsonData;
+              });
+
+              let data = rawData;
+              if (transformer) {
+                data = transformer(rawData);
               }
 
-              compilation.emitAsset(htmlFilePath, new RawSource(html));
+              const htmlFilePath = `${target}index.html`;
+              const jsonFilePath = `${target}data.json`;
+
+              ejs.renderFile(template, data, (err, html) => {
+                if (err) {
+                  throw err;
+                }
+
+                compilation.emitAsset(htmlFilePath, new RawSource(html));
+              });
             });
           }
         );
